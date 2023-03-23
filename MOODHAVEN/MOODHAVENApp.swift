c1b915ef61476c6ae3ad762ec2a73aa12a93d864
Berkay-Disli <berkaydisli.work@gmail.com>
2023-03-23 13:54:32 +0300

    Initial Commit

---
+++ MOODHAVEN/MOODHAVENApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOODHAVENApp.swift
+//  MOODHAVEN
+//
+//  Created by Berkay Disli on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MOODHAVENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
